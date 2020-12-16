8086ce7158b830759b703c282a49f4e007d9e635
Atrem Sergieiev <a_serg83@ukr.net>
2020-12-16 22:40:27 +0100

    DONE!

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './styles.css';
-console.log('HW 11');
+
 const colors = [
   '#FFFFFF',
   '#2196F3',
@@ -8,3 +8,34 @@ const colors = [
   '#009688',
   '#795548',
 ];
+
+let intervalId = null;
+
+const srartRef = document.querySelector('#start');
+const stopRef = document.querySelector('#stop');
+const bodyRef = document.querySelector('body');
+
+srartRef.addEventListener('click', startColorSwitchHendler);
+stopRef.addEventListener('click', stoptColorSwitchHendler);
+
+function startColorSwitchHendler() {
+  srartRef.setAttribute('disabled', 'disabled');
+  intervalId = setInterval(() => {
+    bodyRef.style.backgroundColor = randomColor(colors);
+    console.log(randomColor(colors)); // для отслеживания цвета в консоли
+  }, 1000);
+}
+
+function stoptColorSwitchHendler() {
+  srartRef.removeAttribute('disabled');
+  clearInterval(intervalId);
+  console.log('STOP setInterval');
+}
+
+function randomColor(arr) {
+  return arr[randomIntegerFromInterval(arr.length - 1)];
+}
+
+function randomIntegerFromInterval(max, min = 0) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
